22e80670598703d2b8f032d5fdc1b5f8a2f9b6c0
julianshen <julianshen@gmail.com>
2013-12-09 02:28:56 +0800

    add doc, reformat codes

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 SparkGo
 =======
+Example usage
+-------
+````go
+package main
+
+import (
+   spark "github.com/julianshen/SparkGo"
+   "time"
+)
+
+
+func main() {
+    core := spark.SparkCore{"YOUR_ACCESS_TOKEN","DEVICE_ID"}
+ 
+    c := time.Tick(1 * time.Second)
+    b := true
+    for _ = range c {
+        core.DigitalWrite("D7", b)
+        b=!b
+    }
+}
+````
--- SparkGo.go
+++ SparkGo.go
@@ -1,41 +1,41 @@
 package SparkGo
 
 import (
-    "net/http"
-    "net/url"
     "encoding/json"
     "log"
+    "net/http"
+    "net/url"
     "strconv"
 )
 
 type SparkCore struct {
     Access_token string
-    Device_id string
+    Device_id    string
 }
 
 type TinkerResult struct {
-    Id string `json:id`
-    Name string `json:name`
-    Connected bool `json:connected`
-    ReturnVal int `json:return_value`
+    Id        string `json:id`
+    Name      string `json:name`
+    Connected bool   `json:connected`
+    ReturnVal int    `json:return_value`
 }
 
 const (
-    END_POINT_URL="https://api.spark.io/v1/devices/"
+    END_POINT_URL = "https://api.spark.io/v1/devices/"
 )
 
-func (core *SparkCore)post(command string, params string) (*TinkerResult, error) {
+func (core *SparkCore) post(command string, params string) (*TinkerResult, error) {
     api_url := END_POINT_URL + core.Device_id + "/" + command
 
     values := make(url.Values)
     values.Set("access_token", core.Access_token)
     values.Set("params", params)
 
-    r, err := http.PostForm(api_url, values);
+    r, err := http.PostForm(api_url, values)
 
     if err != nil {
-         log.Printf("error: %s", err)
-         return nil, err
+        log.Printf("error: %s", err)
+        return nil, err
     }
 
     var result TinkerResult
@@ -43,64 +43,62 @@ func (core *SparkCore)post(command string, params string) (*TinkerResult, error)
     err = decoder.Decode(&result)
 
     if err != nil {
-         return nil,err
+        return nil, err
     }
 
     return &result, nil
 }
 
-func (core *SparkCore)DigitalWrite(pin string, val bool) (int, error) {
+func (core *SparkCore) DigitalWrite(pin string, val bool) (int, error) {
     params := pin + ","
 
     if val {
-        params+="HIGH"
+        params += "HIGH"
     } else {
-        params+="LOW"
+        params += "LOW"
     }
 
-    r,err := core.post("digitalwrite", params)
+    r, err := core.post("digitalwrite", params)
 
-    if err!= nil {
+    if err != nil {
         return -1, err
     }
 
     return r.ReturnVal, nil
 }
 
-func (core *SparkCore)DigitalRead(pin string) (int, error) {
+func (core *SparkCore) DigitalRead(pin string) (int, error) {
     params := pin
 
-    r,err := core.post("digitalread", params)
+    r, err := core.post("digitalread", params)
 
-    if err!= nil {
+    if err != nil {
         return -1, err
     }
 
     return r.ReturnVal, nil
 }
 
-func (core *SparkCore)AnalogWrite(pin string, val int) (int, error) {
+func (core *SparkCore) AnalogWrite(pin string, val int) (int, error) {
     params := pin + "," + strconv.Itoa(val)
 
-    r,err := core.post("analogwrite", params)
+    r, err := core.post("analogwrite", params)
 
-    if err!= nil {
+    if err != nil {
         return -1, err
     }
 
     return r.ReturnVal, nil
 }
 
-func (core *SparkCore)AnalogRead(pin string) (int, error) {
+func (core *SparkCore) AnalogRead(pin string) (int, error) {
     params := pin
 
-    r,err := core.post("analogread", params)
+    r, err := core.post("analogread", params)
 
-    if err!= nil {
+    if err != nil {
         return -1, err
     }
 
     return r.ReturnVal, nil
 }
-
-
